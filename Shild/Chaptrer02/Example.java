49e9ab0c9fcfc0bcfbab9452bcf7ad32827a08c6
Evgeniy46 <mafiki2@mail.ru>
2016-09-13 14:40:31 +0300

    Create Example.java

---
+++ Shild/Chaptrer02/Example.java
@@ -0,0 +1,8 @@
+package com.company;
+public class Exemple {
+    //Эта программа начинается с вызова метода main
+    public static void main (String args[]){
+        System.out.println("Простая программа на Java");
+    }
+
+}
