c096178f1837ca68f42fbe3c60ed6ecb1f056637
Evgeniy46 <mafiki2@mail.ru>
2016-09-13 14:47:19 +0300

    Update CharDemo.java

--- Shild/Chapter03/CharDemo.java
+++ Shild/Chapter03/CharDemo.java
@@ -3,7 +3,7 @@ public class CharDemo {
     public static void main(String args[]) {
         char ch1, ch2;
 
-        ch1 = 88;
+        ch1 = 88;//код символа х
         ch2 = 'Y';
 
         System.out.print("ch1 и ch2: ");
