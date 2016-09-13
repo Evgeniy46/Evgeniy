dd160b2ddf0845266488578b6f459467124b0009
Evgeniy46 <mafiki2@mail.ru>
2016-09-13 14:38:54 +0300

    Update BlockTest.java

--- Shild/Chaptrer02/BlockTest.java
+++ Shild/Chaptrer02/BlockTest.java
@@ -4,6 +4,7 @@ public class BlockTest {
     public static void main(String[] args) {
         int x, y;
         y = 20;
+        
         //адресатом этого цикла служит блок кода
         for(x=0; x<10; x++){
             System.out.println("Значение x: " + x);
